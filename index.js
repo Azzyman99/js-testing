511f7e6ae4937e29c8cd5ebb7b56f5e325a4f727
Dylan Miller <91589107+Dylxnmiller@users.noreply.github.com>
2022-03-23 05:19:26 +0000

    03/17 done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Calculator = require("./Calculator.js");//.js is optional
 var calc = new Calculator();
-let result = calc.add(2,1);
-console.log(result);
+let addResult = calc.add(2,1);
+let divideResult = calc.divide(8,0);
+let output = `2+1=${addResult}, 8/0=${divideResult}`;
+console.log(output);
 
